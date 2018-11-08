7db45295bb6a3f3c681a87517e11cbbccdca9f64
Cheng Aihua <chengah@zonekey.com.cn>
2018-11-08 18:14:40 +0800

    修改登录样式

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,39 +1,77 @@
 <template>
-  <div>
-    <div style="display: block">
-      <span>用户名</span>
-      <input v-model="name"/>
-    </div>
-    <div style="display: block">
-      <span>密码</span>
-      <input type="password" v-model="password"/>
-    </div>
-    <input type="submit" value="登录" @click="login()"/>
-    <input type="submit" value="页面跳转" @click="skip()"/>
+    <div class="login">
+        <el-form :model="loginForm" label-width="100px">
+
+            <h1 class="title">
+                登录
+            </h1>
+
+            <el-form-item label="账号" prop="username">
+                <el-input type="password" v-model="loginForm.username" autocomplete="off"></el-input>
+            </el-form-item>
+
+            <el-form-item label="密码" prop="password">
+                <el-input type="password" v-model="loginForm.password" autocomplete="off"></el-input>
+            </el-form-item>
 
 
-  </div>
+            <el-form-item>
+                <el-button type="primary">提交</el-button>
+            </el-form-item>
+        </el-form>
+
+    </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        name: '',//用户名
-        password: ''//密码
-      }
-    },
-    methods: {
-      login() {//登录
-        alert('用户名：' + this.name + '  密码：' + this.password)
-      },
-      skip() {//页面跳转
-        this.$router.push({path: '/home'})
-//        this.$router.replace({path: '/home'})
-      }
+    import ElButton from "../../node_modules/element-ui/packages/button/src/button.vue";
+
+    export default {
+        components: {ElButton},
+        data() {
+            return {
+                //登录表单的数据
+                loginForm: {
+                    username: '',
+                    password: ''
+
+                }
+            };
+        },
+        methods: {
+        }
+
+
     }
-  }
 
 </script>
 <style>
+    html, body {
+        margin: 0;
+        padding: 0;
+    }
+
+    html, body, #app, .login {
+        height: 100%;
+    }
+
+    .login {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .el-form {
+        width: 400px;
+        border: 1px solid #ccc;
+        border-radius: 40px;
+        padding: 0 45px 10px 10px;
+    }
+
+    .el-form .title {
+        font-size: 22px;
+        color: #333;
+        font-weight: bold;
+        padding: 20px 0 20px 60px;
+    }
 
 </style>
