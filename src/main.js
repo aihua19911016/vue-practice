01e91a0a021c5e0cdf8e165482a9b5e9f6ef4cf2
Cheng Aihua <chengah@zonekey.com.cn>
2018-11-09 17:39:16 +0800

    配置axios

--- src/main.js
+++ src/main.js
@@ -7,13 +7,17 @@ import router from './router/router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import axios from 'axios';
+
+Vue.prototype.axios = axios;
+
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: {App},
+    template: '<App/>'
 });
