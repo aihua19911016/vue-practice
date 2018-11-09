e26c1bc63ab1196e0d84cec2396ea3e4468ba39f
Cheng Aihua <chengah@zonekey.com.cn>
2018-11-09 17:38:19 +0800

    表单验证

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="login">
-        <el-form :model="loginForm" label-width="100px">
+        <el-form :model="loginForm" :rules="loginRules" ref="loginForm" label-width="100px">
 
             <h1 class="title">
                 登录
             </h1>
 
             <el-form-item label="账号" prop="username">
-                <el-input type="password" v-model="loginForm.username" autocomplete="off"></el-input>
+                <el-input type="text" v-model="loginForm.username" autocomplete="off"></el-input>
             </el-form-item>
 
             <el-form-item label="密码" prop="password">
@@ -16,7 +16,7 @@
 
 
             <el-form-item>
-                <el-button type="primary">提交</el-button>
+                <el-button type="primary" @click="login()">登录</el-button>
             </el-form-item>
         </el-form>
 
@@ -34,10 +34,42 @@
                     username: '',
                     password: ''
 
+                },
+                //验证规则
+                loginRules: {
+                    username: [
+                        {require: true, message: '请输入用户名', trigger: 'blur'},
+                        {min: 5, max: 18, message: '长度在5到18个字符之间', trigger: 'blur'}
+                    ],
+                    password: [
+                        {require: true, message: '请输入密码', trigger: 'blur'},
+                        {min: 5, max: 18, message: '长度在5到18个字符之间', trigger: 'blur'}
+                    ]
                 }
             };
         },
         methods: {
+            login() {//登录
+                this.$refs.loginForm.validate(valid => {//表单验证
+                    if (valid) {
+                        //保留this
+                        let _this = this;
+                        this.axios.post('/api/checklogin', {
+                            username: this.loginForm.username,
+                            password: this.loginForm.password
+
+                        })
+                            .then((response) => {
+                                console.log('接收后端响应登录的请求数据')
+                            });
+
+                        console.log(this.axios)
+                    } else {
+                        console.log('login err');
+                        return false;
+                    }
+                })
+            }
         }
 
 
